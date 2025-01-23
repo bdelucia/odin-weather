23f32989420e89849e3dab5ad61e5428059f3c62
Robert DeLucia Jr. <bdelucia@asu.edu>
2025-01-22 17:59:01 -0700

    temp color changes

--- src/index.js
+++ src/index.js
@@ -45,5 +45,19 @@ function displayWeatherData(weatherData) {
   const { temp } = weatherData.currentConditions;
   const { description } = weatherData;
   temperatureElement.textContent = temp + '°F';
+  switch (true) {
+    case temp <= 32:
+      temperatureElement.style.color = '#AECBFA';
+      break;
+    case temp > 32 && temp <= 64:
+      temperatureElement.style.color = '#C1E1C1';
+      break;
+    case temp > 64 && temp < 90:
+      temperatureElement.style.color = '#FFD59A';
+      break;
+    case temp >= 90:
+      temperatureElement.style.color = '#FFB3B3';
+      break;
+  }
   descriptionElement.textContent = description;
 }
